a55c4b2e054147c46a970cc88ff1780723fc98e5
inch <sxin2949@naver.com>
2021-01-03 17:07:50 +0900

    3-7 게시글 구

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,15 +1,78 @@
-import {Form, Input, Button} from 'antd';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Card, Button, Avatar, Popover, List, Comment } from 'antd';
+import { RetweetOutlined, HeartTwoTone, HeartOutlined, MessageOutlined, EllipsisOutlined  } from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import PostImages from './PostImages';
+import PostCardContent from './PostCardContent';
 
-const PostCard = () => {
+const PostCard = ({post}) => {
     
-    
-    return (
+    const { user } = useSelector((state)=> state.user);
+    const id = user && user?.id;  // 아이디가 있으면 아이디가 들어가고 없으면 undefined로 들어가게 함 
+    //const { user } = useSelector((state)=> state.user?.id);  // 이게 옵션얼 체이닝이라고 한다. 
+    const [liked, setLiked] = useState(false);
+
+    const onToggleLike = useCallback(() => {
+      setLiked((prev) => !prev);
+    }, []);
+    const [commentFormOpened, setCommentFormOpened] = useState(false);
+    const onToggleComment = useCallback(() => {
+        setCommentFormOpened((prev) => !prev);
+    }, []);
+
+     return (
         <>
-            Postcard
+            <Card
+                cover={post.Images[0] && <PostImages images={post.Images} />}
+                actions={[
+                    <RetweetOutlined key="retweet" />,
+                    liked
+                        ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
+                        : <HeartOutlined key="heart" onClick={onToggleLike} />,
+                    <MessageOutlined key="message" onClick={onToggleComment} />,
+                    <Popover
+                        key="ellipsis"
+                        content={(
+                        <Button.Group>
+                            {id && post.User.id === id
+                            ? (
+                                <>
+                                <Button>수정</Button>
+                                <Button type="danger">삭제</Button>
+                                </>
+                            )
+                            : <Button>신고</Button>}
+                        </Button.Group>
+                        )}
+                    >
+                        <EllipsisOutlined />
+                    </Popover>,
+                ]}
+            >
+                
+                <Card.Meta
+                    avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+                    title={post.User.nickname}
+                    description={<PostCardContent postData={post.content} />}
+                />
+            </Card>
+{/*             <CommentForm />
+            <Comments /> */}
         </>
     )
 }
+
+PostCard.propTypes = {
+    post: PropTypes.shape({
+      id: PropTypes.number,
+      User: PropTypes.object,
+      content: PropTypes.string,
+      createdAt: PropTypes.object,
+      Comments: PropTypes.arrayOf(PropTypes.any),
+      Images: PropTypes.arrayOf(PropTypes.any),
+    }),
+  };
+  
 
 export default PostCard;
---
+++ prepare/front/components/PostCardContent.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const PostCardContent = () => {
+    return (
+        <div>
+            구현 중...
+        </div>
+    )
+};
+
+/* PostImages.propTypes = {
+    images: PropTypes.arrayOf(PropTypes.object),
+} */
+
+export default PostCardContent;

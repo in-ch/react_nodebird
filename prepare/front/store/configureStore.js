23f4b1224f276e0684f946e6fd1648458e77afda
inch <sxin2949@naver.com>
2021-01-09 22:34:12 +0900

    4-1 redux-thunk 이해하기

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,10 +1,17 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { compose, createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
+import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+
+const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
+    console.log(action);
+    return next(action);
+};   // 이렇게 미들웨어를 커스터마이징할 수 있음. 
+
 const configureStore = () => {
-    const middelwares = [];
+    const middelwares = [thunkMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middelwares))
     : composeWithDevTools(applyMiddleware(...middelwares))

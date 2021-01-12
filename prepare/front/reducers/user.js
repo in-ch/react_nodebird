0c8700d8df46d532739a742187137ebf625b2957
inch <sxin2949@naver.com>
2021-01-12 23:05:23 +0900

    음...

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -1,8 +1,10 @@
 export const initialState = {
-    isLoggedIn: false,
-    user: null,
-    signUpdata: {},
-    loginData: {},
+    logInLoading: false, // 로그인 시도중
+    logInDone: false,
+    logInError: null,
+    logOutLoading: false, // 로그아웃 시도중
+    logOutDone: false,
+    logOutError: null,
 };
 
 export const login = (data) => {
@@ -20,18 +22,34 @@ export const logoutAction = (data) => {
 
 const reducer = ((state=initialState,action)=>{
     switch (action.type){
-        case 'LOG_IN':
-            return {
-                ...state,
-                isLoggedIn: true,
-                user: action.data,
-            }
-        case 'LOG_OUT':
-            return {
-                ...state,
-                isLoggedIn: false,
-                user: null,
-            }
+        case LOG_IN_REQUEST:
+            draft.logInLoading = true;
+            draft.logInError = null;
+            draft.logInDone = false;
+            break;
+          case LOG_IN_SUCCESS:
+            draft.logInLoading = false;
+            draft.me = dummyUser(action.data);
+            draft.logInDone = true;
+            break;
+          case LOG_IN_FAILURE:
+            draft.logInLoading = false;
+            draft.logInError = action.error;
+            break;
+          case LOG_OUT_REQUEST:
+            draft.logOutLoading = true;
+            draft.logOutError = null;
+            draft.logOutDone = false;
+            break;
+          case LOG_OUT_SUCCESS:
+            draft.logOutLoading = false;
+            draft.logOutDone = true;
+            draft.me = null;
+            break;
+          case LOG_OUT_FAILURE:
+            draft.logOutLoading = false;
+            draft.logOutError = action.error;
+            break;
         default:
             return state;
     }
--- prepare/sagas/index.js
+++ prepare/sagas/index.js
@@ -1,59 +1,15 @@
 import { all, fork, call, take, put, delay } from 'redux-saga/effects';
+import postSaga from './post';
+import userSaga from './user';
 
-function* logIn(action) {
-    try {
-      console.log('saga logIn');
-      // const result = yield call(logInAPI);
-      yield delay(1000);
-      yield put({
-        type: LOG_IN_SUCCESS,
-        data: action.data,
-      });
-    } catch (err) {
-      console.error(err);
-      yield put({
-        type: LOG_IN_FAILURE,
-        error: err.response.data,
-      });
-    }
-  }
-  
-  function logOutAPI() {
-    return axios.post('/api/logout');
-  }
-  
-  function* logOut() {
-    try {
-      // const result = yield call(logOutAPI);
-      yield delay(1000);
-      yield put({
-        type: LOG_OUT_SUCCESS,
-      });
-    } catch (err) {
-      console.error(err);
-      yield put({
-        type: LOG_OUT_FAILURE,
-        error: err.response.data,
-      });
-    }
-  }
 
-function* watchLogIn() {  // 이벤트 리스너 역활을 함. 
-    yield take('LOG_IN_REQUEST');
-}
 
-function* watchLogOut() {
-    yield take('LOG_OUT_REQUEST');
-}
 
-function* watchAddPost() {
-    yield take('ADD_POST_REQUEST');
-}
+
 
 export default function* rootSaga() {
     yield all([  //all은 아래것 모두 실행하는 것이다. fork는 함수를 실행한다는 것이다.
-        fork(watchLogIn),
-        fork(watchLogOut),
-        fork(watchAddPost),
+        fork(postSaga),
+        fork(userSaga),
     ]);
 }
